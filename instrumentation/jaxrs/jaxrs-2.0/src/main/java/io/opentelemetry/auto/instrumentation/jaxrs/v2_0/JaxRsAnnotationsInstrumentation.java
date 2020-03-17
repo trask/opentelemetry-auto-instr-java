35685d7f8977d8a6a324726a65e83b9a6dd1a0d2
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-03-17 14:15:58 -0700

    Add option to suppress controller and view spans

--- instrumentation/jaxrs/jaxrs-2.0/src/main/java/io/opentelemetry/auto/instrumentation/jaxrs/v2_0/JaxRsAnnotationsInstrumentation.java
+++ instrumentation/jaxrs/jaxrs-2.0/src/main/java/io/opentelemetry/auto/instrumentation/jaxrs/v2_0/JaxRsAnnotationsInstrumentation.java
@@ -29,8 +29,10 @@ import static net.bytebuddy.matcher.ElementMatchers.named;
 
 import com.google.auto.service.AutoService;
 import io.opentelemetry.auto.bootstrap.InstrumentationContext;
+import io.opentelemetry.auto.config.Config;
 import io.opentelemetry.auto.instrumentation.api.SpanWithScope;
 import io.opentelemetry.auto.tooling.Instrumenter;
+import io.opentelemetry.trace.DefaultSpan;
 import io.opentelemetry.trace.Span;
 import java.lang.reflect.Method;
 import java.util.Map;
@@ -100,6 +102,11 @@ public final class JaxRsAnnotationsInstrumentation extends Instrumenter.Default 
         @Advice.This final Object target, @Advice.Origin final Method method) {
       // Rename the parent span according to the path represented by these annotations.
       final Span parent = TRACER.getCurrentSpan();
+
+      if (!Config.get().isExperimentalControllerAndViewSpansEnabled()) {
+        DECORATE.onJaxRsSpan(null, parent, target.getClass(), method);
+        return new SpanWithScope(DefaultSpan.getInvalid(), null);
+      }
 
       final Span span = TRACER.spanBuilder(JAX_ENDPOINT_OPERATION_NAME).startSpan();
       DECORATE.onJaxRsSpan(span, parent, target.getClass(), method);
